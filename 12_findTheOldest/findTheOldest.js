cc28e3644b481e64df4da3e076d5b54c39900063
dittygoops <aditya.gup16@gmail.com>
2023-04-11 22:27:52 -0700

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const sorted = people.sort((lastPerson, nextPerson) => {
+        let currentYear = new Date().getFullYear();
 
+        let lastAge = 0;
+        if (lastPerson.yearOfDeath) {
+            lastAge = lastPerson.yearOfDeath - lastPerson.yearOfBirth;
+        } else {
+            lastAge = currentYear - lastPerson.yearOfBirth;
+        }
+
+        let nextAge = 0;
+        if (nextPerson.yearOfDeath) {
+            nextAge = nextPerson.yearOfDeath - nextPerson.yearOfBirth;
+        } else {
+            nextAge = currentYear - nextPerson.yearOfBirth;
+        }
+
+        return lastAge < nextAge ? 1 : -1;
+    });
+
+    return sorted[0];
 };
 
 // Do not edit below this line
