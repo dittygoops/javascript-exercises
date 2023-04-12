65a53e9086597aaa8d0400333f097c7744e454c5
dittygoops <aditya.gup16@gmail.com>
2023-04-11 22:12:56 -0700

    Complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let num = parseInt(n);
 
+    if (num < 1) {
+        return "OOPS";
+    } else {
+        if (num === 1 || num === 2) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
 };
 
 // Do not edit below this line
